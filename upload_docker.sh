921dead4bf339d0fa282b1f80edc5da6ed117c03
saibodicherla <saibodicherla@gmail.com>
2020-07-21 02:33:11 +0200

    🚀 feat(push image): Upload a Docker Image to HUB

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=saibodicherla/mlproject
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=saibodicherla
+docker tag ba240ad962fc saibodicherla/mlproject:v.01
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push saibodicherla/mlproject
